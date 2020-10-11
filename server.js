a904e8b414df12729c9f7caf6302f6f1f3b09bf1
mohsen <mohsen.workman@gmail.com>
2020-10-11 10:52:05 +0330

    add requirement files to handle start project with npm start script

---
+++ server.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' });
+  fs.createReadStream('src/index.html').pipe(res);
+
+});
+
+server.listen(process.env.PORT || 3000);
