2b1a6d974d26f8ed763a9a4a77f451765047faf2
mohsen <mohsen.workman@gmail.com>
2020-12-02 14:31:50 +0330

    set kashf element in center verticaly; set product-arrow hiddenable

--- src/js/kashf.js
+++ src/js/kashf.js
@@ -12,24 +12,24 @@ $(document).ready(function () {
   const id5 = 'kashf-img-5';
 
   $("#kashf-text-1").addClass('active');
-  carousel.on('slide.bs.carousel', function (){
+  carousel.on('slide.bs.carousel', function () {
     const id = $('#kashf-carousel .active').attr('id');
     switch (id) {
-     case id1:
-       text1.removeClass('active');
-       break;
-     case id2:
-       text2.removeClass('active');
-       break;
-     case id3:
-       text3.removeClass('active');
-       break;
-     case id4:
-       text4.removeClass('active');
-       break;
-     case id5:
-       text5.removeClass('active')
-     }
+      case id1:
+        text1.removeClass('active');
+        break;
+      case id2:
+        text2.removeClass('active');
+        break;
+      case id3:
+        text3.removeClass('active');
+        break;
+      case id4:
+        text4.removeClass('active');
+        break;
+      case id5:
+        text5.removeClass('active')
+    }
   });
 
   carousel.on('slid.bs.carousel', function () {
@@ -52,3 +52,24 @@ $(document).ready(function () {
     }
   });
 });
+
+const scroll_func = function toggle_show_arrow() {
+  const windowInnerWidth = window.innerWidth;
+  const allowable_px_dive_to_kashf = (4/100)*windowInnerWidth;
+  var element_offset = $('#kashf-element').offset().top + allowable_px_dive_to_kashf;
+  var window_h = $(window).height();
+  var product_arrow = $('.product-arrow');
+  var scroll_from_top = $(this).scrollTop();
+  var should_be_hidden = !product_arrow.hasClass('invisible')
+      && scroll_from_top > (element_offset + allowable_px_dive_to_kashf - window_h);
+  var should_be_show = product_arrow.hasClass('invisible')
+      && window_h < element_offset + allowable_px_dive_to_kashf
+      && scroll_from_top < (element_offset + allowable_px_dive_to_kashf - window_h);
+  if (should_be_hidden) {
+    product_arrow.addClass('invisible')
+  } else if (should_be_show) {
+    product_arrow.removeClass('invisible')
+  }
+};
+
+window.addEventListener('scroll', scroll_func,  {passive: true});
