aa7961309c0258312982bb8ecf5620a1289ede62
mohsen <mohsen.workman@gmail.com>
2021-01-14 13:03:41 +0330

    responsive hazm code

--- src/js/kashf.js
+++ src/js/kashf.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   sync_kashf_text_with_kashfCarousel();
-  typeWriter()
+  add_line()
 });
 
 // for remove arrow in scrolling
 const scroll_func = function toggle_show_arrow() {
   const windowInnerWidth = window.innerWidth;
-  const allowable_px_dive_to_kashf = windowInnerWidth > 991 ? (4/100)*windowInnerWidth : 0;
+  const allowable_px_dive_to_kashf = windowInnerWidth > 991 ? (4 / 100) * windowInnerWidth : 0;
   var element_offset = $('#kashf-element').offset().top + allowable_px_dive_to_kashf;
   var window_h = $(window).height();
   var product_arrow = $('.product-arrow');
@@ -22,7 +22,7 @@ const scroll_func = function toggle_show_arrow() {
     product_arrow.removeClass('invisible')
   }
 };
-window.addEventListener('scroll', scroll_func,  {passive: true});
+window.addEventListener('scroll', scroll_func, {passive: true});
 
 
 // handle kashf-text show and swap text synchronous with kashf-carousel
@@ -85,86 +85,61 @@ function sync_kashf_text_with_kashfCarousel() {
   });
 }
 
-var text_el = document.querySelector('.hazm-code-inner div');
-var hazm_code_elements = '<span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="kn">from&nbsp;</span>|<span class="nn">__future__&nbsp;</span>|<span class="kn">import&nbsp;</span>|<span class="n">unicode_literals</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="kn">from&nbsp;</span>|<span class="nn">hazm&nbsp;</span>|<span class="kn">import&nbsp;</span>|<span class="o">*</span>|\n' +
-    '              <br><br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">normalizer&nbsp;</span>|<span class="o">=&nbsp;</span>|<span class="n">Normalizer</span>|<span class="p">()</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">normalizer</span>|<span class="o">.</span>|<span class="n">normalize</span>|\n' +
-    '              <span class="p">(</span>|<span class="s">\'اصلاح&nbsp;</span>|<span class="s">نویسه ها&nbsp;</span>|<span class="s">و&nbsp;</span>|<span class="s">استفاده&nbsp;</span>|<span class="s">از&nbsp;</span>|<span class="s">نیم&nbsp;</span>|<span class="s">فاصله&nbsp;</span>|<span class="s">پردازش&nbsp;</span>|<span class="s">را&nbsp;</span>|<span class="s">آسان&nbsp;</span>|<span class="s">می کند\'</span>|<span class="p">)</span>|\n' +
-    '              <br>\n' +
-    '              <span class="s">\'اصلاح&nbsp;</span>|<span class="s">نویسه ها&nbsp;</span>|<span class="s">و&nbsp;</span>|<span class="s">استفاده&nbsp;</span>|<span class="s">از&nbsp;</span>|<span class="s">نیم&nbsp;</span>|<span class="s">فاصله&nbsp;</span>|<span class="s">پردازش&nbsp;</span>|<span class="s">را&nbsp;</span>|<span class="s">آسان&nbsp;</span>|<span class="s">می کند\'</span>|\n' +
-    '              <br><br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">sent_tokenize</span>|<span class="p">(</span>|<span class="s">\'ما&nbsp;</span>|<span class="s">هم&nbsp;</span>|<span class="s">برای&nbsp;</span>|<span class="s">وصل&nbsp;</span>|<span class="s">کردن&nbsp;</span>|<span class="s">آمدیم</span>|<span class="s">!&nbsp;</span>|<span class="s">ولی&nbsp;</span>|<span class="s">برای&nbsp;</span>|<span class="s">پردازش</span>|<span class="s">،&nbsp;</span>|<span class="s">جدا&nbsp;</span>|<span class="s">بهتر&nbsp;</span>|<span class="s">نیست</span>|<span class="s">؟\'</span>|<span class="p">)</span>|' +
-    '              <br>\n' +
-    '              <span class="p">[</span>|<span class="s">\'ما&nbsp;</span>|<span class="s">هم&nbsp;</span>|<span class="s">برای&nbsp;</span>|<span class="s">وصل&nbsp;</span>|<span class="s">کردن&nbsp;</span>|<span class="s">آمدیم</span>|<span class="s">!</span>|'+
-    '              <span class="p">,</span>|\n'+
-    '              <span class="s">ولی&nbsp;</span>|<span class="s">برای&nbsp;</span>|<span class="s">پردازش</span>|<span class="s">،&nbsp;</span>|<span class="s">جدا&nbsp;</span>|<span class="s">بهتر&nbsp;</span>|<span class="s">نیست</span>|<span class="s">؟\'</span>|<span class="p">]</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">word_tokenize</span>|\n' +
-    '              <span class="p">(</span>|<span class="s">\'</span>|<span class="s">ولی&nbsp;</span>|<span class="s">برای&nbsp;</span>|<span class="s">پردازش</span>|<span class="s">،&nbsp;</span>|<span class="s">جدا&nbsp;</span>|<span class="s">بهتر&nbsp;</span>|<span class="s">نیست</span>|<span class="s">؟\'</span>|<span class="p">)</span>|\n' +
-    '              <br>\n' +
-    '              <span class="p">[</span>|<span class="s">\'ولی\'</span>|<span class="p">,&nbsp;</span>|<span class="s">\'برای\'</span>|<span class="p">,&nbsp;</span>|<span class="s">\'پردازش\'</span>|<span class="p">،&nbsp;</span>|<span class="s">\'جدا\'</span>|<span class="p">,&nbsp;</span>|<span class="s">\'بهتر\'</span>|<span class="p">,&nbsp;</span>|<span class="s">\'نیست\'</span>|<span class="p">,&nbsp;</span>|<span class="s">\'؟\'</span>|<span class="p">]</span>|\n' +
-    '              <br><br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">stemmer&nbsp;</span>|<span class="o">=&nbsp;</span>|<span class="n">Stemmer</span>|<span class="p">()</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">stemmer</span>|<span class="o">.</span>|<span class="n">stem</span>|<span class="p">(</span>|<span class="s">\'کتاب&zwnj;ها\'</span>|<span class="p">)</span>|' +
-    '              <br>\n' +
-    '              <span class="s">\'کتاب\'</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">lemmatizer&nbsp;</span>|<span class="o">=&nbsp;</span>|<span class="n">Lemmatizer</span>|<span class="p">()</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">lemmatizer</span>|<span class="o">.</span>|<span class="n">lemmatize</span>|<span class="p">(</span>|<span class="s">\'می&zwnj;روم\'</span>|<span class="p">)</span>|\n' +
-    '              <br>\n' +
-    '              <span class="s">\'</span>|<span class="s">رفت</span>|<span class="s">#</span>|<span class="s">رو</span>|<span class="s">\'</span>|\n' +
-    '              <br><br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">tagger&nbsp;</span>|<span class="o">=&nbsp;</span>|<span class="n">POSTagger</span>|<span class="p">(</span>|<span class="n">model</span>|<span class="o">=</span>|\n' +
-    '              <span class="s">\'</span>|<span class="s">resources</span>|<span class="s">/</span>|<span class="s">postagger</span>|<span class="s">.</span>|<span class="s">model</span>|<span class="s">\'</span>|<span class="p">)</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">tagger</span>|<span class="o">.</span>|<span class="n">tag</span>|<span class="p">(</span>|<span class="n">word_tokenize</span>|<span class="p">(</span>|<span class="s">\'</span>|<span class="s">ما&nbsp;</span>|<span class="s">بسیار&nbsp;</span>|<span class="s">کتاب&nbsp;</span>|<span class="s">می خوانیم</span>|<span class="s">\'</span>|<span class="p">))</span>|\n' +
-    '              <br>\n' +
-    '              <span class="p">[(</span>|<span class="s">\'ما\'</span>|<span class="p">,</span>|<span class="s">\'PRO\'</span>|<span class="p">),</span>|<span class="p">(</span>|<span class="s">\'بسیار\'</span>|<span class="p">,</span>|<span class="s">\'ADV\'</span>|<span class="p">),</span>|<span class="p">(</span>|<span class="s">\'کتاب\'</span>|<span class="p">,</span>|<span class="s">\'N\'</span>|<span class="p">),</span>|<span class="p">(</span>|<span class="s">\'می خوانیم\'</span>|<span class="p">,</span>|<span class="s">\'V\'</span>|<span class="p">)]</span>|\n' +
-    '              <br><br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">chunker&nbsp;</span>|<span class="o">=&nbsp;</span>|<span class="n">Chunker</span>|<span class="p">(</span>|<span class="n">model</span>|<span class="o">=</span>|<span class="s">\'resources</span>|<span class="s">/</span>|<span class="s">chunker</span>|<span class="s">.</span>|<span class="s">model\'</span>|<span class="p">)</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">tagged&nbsp;</span>|<span class="o">=&nbsp;</span>|<span class="n">tagger</span>|<span class="o">.</span>|<span class="n">tag</span>|<span class="p">(</span>|<span class="n">word_tokenize</span>|<span class="p">(</span>|<span class="s">\'کتاب&nbsp;</span>|<span class="s">خواندن&nbsp;</span>|<span class="s">را&nbsp;</span>|<span class="s">دوست&nbsp;</span|<span class="s">داریم\'</span>|<span class="p">))</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">tree2brackets</span>|<span class="p">(</span>|<span class="n">chunker</span>|<span class="o">.</span>|<span class="n">parse</span>|<span class="p">(</span>|<span class="n">tagged</span>|<span class="p">))</span>|' +
-    '              <br>\n' +
-    '              <span class="s">\'[</span><span class="s">کتاب&nbsp;</span>|<span class="s">خواندن&nbsp;</span>|<span class="s">NP</span>|<span class="s">]&nbsp;</span>|<span class="s">[</span>|<span class="s">را&nbsp;</span>|<span class="s">POSTP</span>|<span class="s">]&nbsp;</span>|<span class="s">[</span>|<span class="s">دوست&nbsp;</span>|<span class="s">داریم&nbsp;</span>|<span class="s">VP</span>|<span class="s">]\'</span>|\n' +
-    '              <br><br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">parser&nbsp;</span>|<span class="o">=&nbsp;</span>|<span class="n">DependencyParser</span>|<span class="p">(</span>|<span class="n">tagger&nbsp;</span>|<span class="o">=&nbsp;</span>|<span class="n">tagger</span>|<span class="p">,</span>| <span class="n">lemmatizer</span>|<span class="o">=</span>|<span class="n">lemmatizer</span>|<span class="p">)</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&gt;&gt;&gt;&nbsp;</span>|<span class="n">parser</span>|<span class="o">.</span>|<span class="n">parse</span>|<span class="p">(</span>|<span class="n">word_tokenize</span>|<span class="p">(</span>|<span class="s">\'</span>|<span class="s">زنگ ها&nbsp;</span>|<span class="s">برای&nbsp;</span>|<span class="s">کی&nbsp;</span>|<span class="s">به&nbsp;</span>|<span class="s">صدا&nbsp;</span>|<span class="s">در&nbsp;</span>|<span class="s">می آید</span>|<span class="s">؟\'</span>|<span class="p">))</span>|\n' +
-    '              <br>\n' +
-    '              <span class="o">&lt;</span>|<span class="n">DependencyGraph&nbsp;</span>|<span class="k">with&nbsp;</span>|<span class="mi">8&nbsp;</span>|<span class="n">nodes</span>|<span class="o">&gt;</span>|\n' +
-    '            </div>';
 
+// handle show code as type writer in hazm
+var lines = $('.lines');
+const lines_array = Array.from(lines[0].children);
 var i = 0;
+var j = 0;
 var t;
+var line = [];
 var speed = 50;
-const hazm_code_elements_array = hazm_code_elements.split("|");
+const height_of_lines = lines.height();
 
-function sleep(ms) {
-  return new Promise(resolve => {t = setTimeout(typeWriter, ms)});
+lines.empty();
+lines.height(height_of_lines);
+
+function sleep_for_addChild(ms, line_children) {
+  return new Promise(resolve => {
+    t = setTimeout(function () {
+      add_line_child(line_children);
+    }, ms)
+  }).then(function () {
+    clearTimeout(t);
+  });
 }
 
-function stop_time_func() {
-  clearTimeout(t);
-  i = 0;
-  text_el.innerHTML = '';
+function sleep_for_addLine(ms) {
+  return new Promise(resolve => {
+    t = setTimeout(add_line, ms)
+  }).then(function () {
+    clearTimeout(t);
+  });
 }
 
-async function typeWriter() {
-  if (i < hazm_code_elements_array.length) {
-    text_el.innerHTML +=hazm_code_elements_array[i];
-    i++;
-    await sleep(speed);
+async function add_line_child(line_children) {
+  if (j < line_children.length) {
+    $(".lines div").last().append(line_children[j].outerHTML);
+    j++;
+    await sleep_for_addChild(speed, line_children);
+  } else {
+    j = 0;
+    await sleep_for_addLine(speed)
   }
-  if (i === hazm_code_elements_array.length) {
-    stop_time_func();
-    sleep(0)
+}
+
+async function add_line() {
+  if (i < lines_array.length) {
+    line = lines_array[i];
+    var className = line ? line.className : '';
+    var line_children = Array.from(line ? line.children : []);
+    lines.append('<div class="' + className + '"></div>');
+    i++;
+    await add_line_child(line_children);
+  }
+  else {
+    i = 0;
+    lines.empty();
+    await sleep_for_addLine()
   }
 }
