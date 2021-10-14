aca0ab3fda55b6300e8b2155b68ac60c78326e8a
Muhammad <muhammad.ksht@gmail.com>
2021-10-14 23:30:52 +0330

    added title

--- new-website-src/src/components/slides/customers/customers-mobile.js
+++ new-website-src/src/components/slides/customers/customers-mobile.js
@@ -28,6 +28,7 @@ import {ReactComponent as Nazdika} from "../../../assets/images/mobile/customers
 import {ReactComponent as Amin} from "../../../assets/images/mobile/customers/row2/couple1/Amin.svg";
 import {ReactComponent as Ammar} from "../../../assets/images/mobile/customers/row2/couple2/Ammar.svg";
 import {ReactComponent as Farabi} from "../../../assets/images/mobile/customers/row2/couple3/Farabi.svg";
+import {ReactComponent as Text} from '../../../assets/images/mobile/customers/text.svg'
 import {
     couple21Man,
     couple21Woman,
@@ -39,151 +40,156 @@ import {
 
 function CustomersMobile(props){
     return(
-        <div id={'table'}>
-            <div className={'row'}>
-                <div className={'box'} id={'box-left'}>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple11Man.initial}
-                        animate={couple11Man.animate}
-                        transition={couple11Man.transition}
-                    >
-                        <Keshvar/>
-                    </motion.div>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple11Woman.initial}
-                        animate={couple11Woman.animate}
-                        transition={couple11Woman.transition}
-                    >
-                        <Jahad/>
-                    </motion.div>
-                </div>
-                <div className={'box'} id={'box-middle'}>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple12Man.initial}
-                        animate={couple12Man.animate}
-                        transition={couple12Man.transition}
-                    >
-                        <Naft/>
-                    </motion.div>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple12Woman.initial}
-                        animate={couple12Woman.animate}
-                        transition={couple12Woman.transition}
-                    >
-                        <Bimeh/>
-                    </motion.div>
-                </div>
-                <div className={'box'} id={'box-right'}>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple13Man.initial}
-                        animate={couple13Man.animate}
-                        transition={couple13Man.transition}
-                    >
-                        <Ketabkhaneh/>
-                    </motion.div>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple13Woman.initial}
-                        animate={couple13Woman.animate}
-                        transition={couple13Woman.transition}
-                    >
-                        <Nazdika/>
-                    </motion.div>
-                </div>
+        <div id={'customers'}>
+            <div id={'text'}>
+                <Text/>
             </div>
-            <div className={'row'}>
-                <div className={'box'} id={'box-left'}>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple21Man.initial}
-                        animate={couple21Man.animate}
-                        transition={couple21Man.transition}
-                    >
-                        <Setad/>
-                    </motion.div>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple21Woman.initial}
-                        animate={couple21Woman.animate}
-                        transition={couple21Woman.transition}
-                    >
-                        <Amin/>
-                    </motion.div>
+            <div id={'table'}>
+                <div className={'row'}>
+                    <div className={'box'} id={'box-left'}>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple11Man.initial}
+                            animate={couple11Man.animate}
+                            transition={couple11Man.transition}
+                        >
+                            <Keshvar/>
+                        </motion.div>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple11Woman.initial}
+                            animate={couple11Woman.animate}
+                            transition={couple11Woman.transition}
+                        >
+                            <Jahad/>
+                        </motion.div>
+                    </div>
+                    <div className={'box'} id={'box-middle'}>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple12Man.initial}
+                            animate={couple12Man.animate}
+                            transition={couple12Man.transition}
+                        >
+                            <Naft/>
+                        </motion.div>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple12Woman.initial}
+                            animate={couple12Woman.animate}
+                            transition={couple12Woman.transition}
+                        >
+                            <Bimeh/>
+                        </motion.div>
+                    </div>
+                    <div className={'box'} id={'box-right'}>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple13Man.initial}
+                            animate={couple13Man.animate}
+                            transition={couple13Man.transition}
+                        >
+                            <Ketabkhaneh/>
+                        </motion.div>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple13Woman.initial}
+                            animate={couple13Woman.animate}
+                            transition={couple13Woman.transition}
+                        >
+                            <Nazdika/>
+                        </motion.div>
+                    </div>
                 </div>
-                <div className={'box'} id={'box-middle'}>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple22Man.initial}
-                        animate={couple22Man.animate}
-                        transition={couple22Man.transition}
-                    >
-                        <Astan/>
-                    </motion.div>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple22Woman.initial}
-                        animate={couple22Woman.animate}
-                        transition={couple22Woman.transition}
-                    >
-                        <Ammar/>
-                    </motion.div>
+                <div className={'row'}>
+                    <div className={'box'} id={'box-left'}>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple21Man.initial}
+                            animate={couple21Man.animate}
+                            transition={couple21Man.transition}
+                        >
+                            <Setad/>
+                        </motion.div>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple21Woman.initial}
+                            animate={couple21Woman.animate}
+                            transition={couple21Woman.transition}
+                        >
+                            <Amin/>
+                        </motion.div>
+                    </div>
+                    <div className={'box'} id={'box-middle'}>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple22Man.initial}
+                            animate={couple22Man.animate}
+                            transition={couple22Man.transition}
+                        >
+                            <Astan/>
+                        </motion.div>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple22Woman.initial}
+                            animate={couple22Woman.animate}
+                            transition={couple22Woman.transition}
+                        >
+                            <Ammar/>
+                        </motion.div>
+                    </div>
+                    <div className={'box'} id={'box-right'}>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple23Man.initial}
+                            animate={couple23Man.animate}
+                            transition={couple23Man.transition}
+                        >
+                            <Mokhaberat/>
+                        </motion.div>
+                        <motion.div
+                            className={'box'}
+                            id={'stack'}
+                            initial={couple23Woman.initial}
+                            animate={couple23Woman.animate}
+                            transition={couple23Woman.transition}
+                        >
+                            <Farabi/>
+                        </motion.div>
+                    </div>
                 </div>
-                <div className={'box'} id={'box-right'}>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple23Man.initial}
-                        animate={couple23Man.animate}
-                        transition={couple23Man.transition}
-                    >
-                        <Mokhaberat/>
-                    </motion.div>
-                    <motion.div
-                        className={'box'}
-                        id={'stack'}
-                        initial={couple23Woman.initial}
-                        animate={couple23Woman.animate}
-                        transition={couple23Woman.transition}
-                    >
-                        <Farabi/>
-                    </motion.div>
+                <div className={'row'}>
+                    <div className={'box'} id={'box-left'}>
+                        <Parsian/>
+                    </div>
+                    <div className={'box'} id={'box-middle'}>
+                        <Irandoc/>
+                    </div>
+                    <div className={'box'} id={'box-right'}>
+                        <Azmoon/>
+                    </div>
                 </div>
-            </div>
-            <div className={'row'}>
-                <div className={'box'} id={'box-left'}>
-                    <Parsian/>
-                </div>
-                <div className={'box'} id={'box-middle'}>
-                    <Irandoc/>
-                </div>
-                <div className={'box'} id={'box-right'}>
-                    <Azmoon/>
-                </div>
-            </div>
-            <div className={'row'}>
-                <div className={'box'} id={'box-left'}>
-                    <Tamasha/>
-                </div>
-                <div className={'box'} id={'box-middle'}>
-                    <Taghche/>
-                </div>
-                <div className={'box'} id={'box-right'}>
-                    <Varzesh/>
+                <div className={'row'}>
+                    <div className={'box'} id={'box-left'}>
+                        <Tamasha/>
+                    </div>
+                    <div className={'box'} id={'box-middle'}>
+                        <Taghche/>
+                    </div>
+                    <div className={'box'} id={'box-right'}>
+                        <Varzesh/>
+                    </div>
                 </div>
             </div>
         </div>
