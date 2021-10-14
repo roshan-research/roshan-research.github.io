a41214de80ba8708ab56972ef6fc68d59e3e9a6b
Muhammad <muhammad.ksht@gmail.com>
2021-10-14 16:11:11 +0330

    added home in mobile menu and fixed sizes

--- new-website-src/src/components/header/header.js
+++ new-website-src/src/components/header/header.js
@@ -3,8 +3,10 @@ import {Component} from 'react';
 import './header.scss'
 import Navbar from "./navigation-bar/navbar";
 import MainPage from "./main-page/main-page";
+import {ReactComponent as HomeButton} from '../../assets/images/home-mobile.svg'
 import JoinUsButton from "./navigation-bar/links-group/join-us-button/join-us-button";
 import ContactUsButton from "./navigation-bar/links-group/contact-us-button/contact-us-button";
+import {Link} from "react-router-dom";
 
 const JoinusPage = lazy(() => import("./join-us-page/joinus-page"));
 const ContactUsPage = lazy(() => import("./contact-us-page/contact-us-page"));
@@ -24,6 +26,9 @@ class Header extends Component {
         if (this.state.isOpen) {
             return (
                 <div id={'compact-menu'}>
+                    <Link to={'/'}>
+                        <HomeButton id={'home'}/>
+                    </Link>
                     <ContactUsButton />
                     <JoinUsButton />
                 </div>
