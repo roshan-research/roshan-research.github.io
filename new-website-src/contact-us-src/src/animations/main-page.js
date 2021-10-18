52f47657290bfacbfdbe226fd3dcd3dd1dbf71b6
Muhammad <muhammad.ksht@gmail.com>
2021-10-18 18:17:11 +0330

    deleted unnecessary files

--- new-website-src/contact-us-src/src/animations/main-page.js
+++ new-website-src/contact-us-src/src/animations/main-page.js
@@ -10,35 +10,6 @@ export const startingAnimation = {
     }
 };
 
-export const textScrollDownAnimation = {
-    initial: {
-        opacity: 1,
-        y:0,
-    },
-    animate: {
-        opacity: 0,
-        y:-150,
-    },
-    transition:{
-        delay:0.4,
-        default: { duration: 0.3},
-    }
-};
-
-
-export const imageScrollDownAnimation = {
-    initial: {
-        opacity: 1,
-    },
-    animate: {
-        opacity: 0,
-    },
-    transition:{
-        default: { duration: 0.3},
-    }
-};
-
-
 export const noneAnimation = {
     initial: {
     },
@@ -46,26 +17,4 @@ export const noneAnimation = {
     },
     transition:{
     }
-};
-
-export const noneAnimationAlefba = {
-    hidden: {
-        x:-33,
-    },
-    visible: {
-        transition: {
-            duration: 0.1
-        }
-    },
-};
-
-export const noneAnimationHazm = {
-    hidden: {
-        y:+25,
-    },
-    visible: {
-        transition: {
-            duration: 0.1
-        }
-    },
 };
