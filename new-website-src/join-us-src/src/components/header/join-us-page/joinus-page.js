590a52e8d20946ec158ef8bcc51b62f12131453f
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-10-30 12:01:30 +0330

    new build versions after replacing the pics

--- new-website-src/join-us-src/src/components/header/join-us-page/joinus-page.js
+++ new-website-src/join-us-src/src/components/header/join-us-page/joinus-page.js
@@ -2,17 +2,7 @@ import React from 'react';
 import JoinUsMobile from "./join-us-mobile";
 import {ReactComponent as JoinUsWeb} from "../../../assets/images/web-join-us.svg";
 import './joinus-page.scss';
-import {motion} from 'framer-motion';
-import {noneAnimation, startingAnimation} from "../../../animations/main-page";
 import {isMobile, isSafari,isOpera,isTablet} from "react-device-detect";
-
-const imageAnimationChooser = (props) => {
-    if(props.shouldRerender){
-        return startingAnimation;
-    } else {
-        return noneAnimation;
-    }
-};
 
 const returnBasedOneDevice = () => {
     if(isTablet){
@@ -38,13 +28,8 @@ const returnBasedOneDevice = () => {
 
 export default function JoinusPage(props) {
     return (
-        <motion.div
-            className={'join-us-page'}
-            initial={imageAnimationChooser(props).initial}
-            animate={imageAnimationChooser(props).animate}
-            transition={imageAnimationChooser(props).transition}
-        >
+        <div className={'join-us-page'}>
             {returnBasedOneDevice()}
-        </motion.div>
+        </div>
     )
 }
