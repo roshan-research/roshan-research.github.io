18480c86350938d1eea2f19b4229462b15039446
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-07 08:00:47 +0330

    fixed the progress indicator bug

--- new-website-src/main-src/src/components/progress-indicator.js
+++ new-website-src/main-src/src/components/progress-indicator.js
@@ -1,12 +1,13 @@
 import Lottie from "lottie-react";
 import progressIndicator from "../assets/lotties/progress-indicator.json";
 import './progress-indicator.css'
+import {isMobile} from "react-device-detect";
 
 function ProgressIndicator(){
     return(
         <div className={'wrapper'}>
-            <div id={'progress-indicator'}>
-                <Lottie animationData={progressIndicator}/>
+            <div id={isMobile ? 'progress-indicator' : ''}>
+                <Lottie id={isMobile ? '' : 'progress-indicator'} animationData={progressIndicator}/>
             </div>
         </div>
     )
