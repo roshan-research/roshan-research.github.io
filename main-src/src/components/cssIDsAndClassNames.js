1b24574289c7994965f9e0de33aaeb1bf95261fb
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-01-14 18:26:11 +0330

    starting a massive refactoring

---
+++ main-src/src/components/cssIDsAndClassNames.js
@@ -0,0 +1,30 @@
+const cssIDsAndClassNames = {
+    kashf: {
+        IDs: {
+            kashfImage: 'kashf-image',
+            kashfText: 'kashf-text',
+        },
+        ClassNames: {
+
+        }
+    },
+    home: {
+        IDs: {
+
+        },
+        ClassNames: {
+
+        }
+    },
+    alefba: {
+        IDs: {
+
+        },
+        ClassNames: {
+
+        }
+    },
+}
+
+
+export default cssIDsAndClassNames;
