a85ba4a0c2de5ab633c1fa58da8a5357beab406a
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-01-29 12:56:30 +0330

    without prerendering

--- main-src/src/index.js
+++ main-src/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import { loadComponents, getState } from "loadable-components";
+import * as ReactDOMM from "react-dom";
 
-import { hydrate, render } from "react-dom";
-
-const rootElement = document.getElementById("root");
-window.snapSaveState = () => getState();
-
-loadComponents()
-    .then(() => hydrate(<App />, rootElement))
-    .catch(() => render(<App />, rootElement));
+ReactDOMM.render(<App/>,document.getElementById('root'));
