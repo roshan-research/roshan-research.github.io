35d35d3cafe29ba5d43c0b0acd39f2e9158c4745
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-02-01 09:11:20 +0330

    ui changes

--- main-src/src/components/header/ContactUsButton.jsx
+++ main-src/src/components/header/ContactUsButton.jsx
@@ -2,7 +2,7 @@ import '../../stylesheets/header-button.scss';
 
 function ContactUsButton() {
     return (
-        <div title={'بخش تماس با ما'} style={{marginTop: '6px'}}>
+        <div title={'بخش تماس با ما'} style={{marginTop: '0.4vw'}}>
             <a href={'https://www.roshan-ai.ir/contact-us'} className={'header-button'}>
                 تماس با ما
             </a>
