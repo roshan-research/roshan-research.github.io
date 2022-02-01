1d465d56f7b502ce7cb69e7178a3c8dee88ee9fa
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-02-01 09:02:46 +0330

    header alignment fixes

--- main-src/src/components/header/ContactUsButton.jsx
+++ main-src/src/components/header/ContactUsButton.jsx
@@ -2,7 +2,7 @@ import '../../stylesheets/header-button.scss';
 
 function ContactUsButton() {
     return (
-        <div title={'بخش تماس با ما'} style={{marginTop: '4px'}}>
+        <div title={'بخش تماس با ما'} style={{marginTop: '6px'}}>
             <a href={'https://www.roshan-ai.ir/contact-us'} className={'header-button'}>
                 تماس با ما
             </a>
--- main-src/src/components/header/JoinUsButton.jsx
+++ main-src/src/components/header/JoinUsButton.jsx
@@ -2,7 +2,7 @@ import '../../stylesheets/header-button.scss'
 
 function JoinUsButton() {
     return (
-        <div title={'بخش همکاری با ما'} style={{marginTop: '4px'}}>
+        <div title={'بخش همکاری با ما'} style={{marginTop: '6px'}}>
             <a href={'https://www.roshan-ai.ir/join-us'} className={'header-button'}>
                 همکاری با ما
             </a>
