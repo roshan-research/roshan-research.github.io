e8ebeb9ffdf6deac433f5b87f438d2c588ad56c7
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-02-01 09:44:47 +0330

    fixed issues

--- main-src/src/components/header/JoinUsButton.jsx
+++ main-src/src/components/header/JoinUsButton.jsx
@@ -2,7 +2,7 @@ import '../../stylesheets/header-button.scss'
 
 function JoinUsButton() {
     return (
-        <div title={'بخش همکاری با ما'} style={{marginTop: '0.4vw'}}>
+        <div title={'بخش همکاری با ما'} style={{marginTop: '0.37vw'}}>
             <a href={'https://www.roshan-ai.ir/join-us'} className={'header-button'}>
                 همکاری با ما
             </a>
