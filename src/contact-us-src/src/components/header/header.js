b420e02a1f9608432c8d91f44e3bc954b58b5b78
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-05-08 12:12:57 +0430

    updated

--- src/contact-us-src/src/components/header/header.js
+++ src/contact-us-src/src/components/header/header.js
@@ -51,7 +51,8 @@ export default class Header extends Component {
                     <img src={mapImage} id={'map-image'} alt={''}/>
                     <a
                         id={'navigation-button'}
-                        href={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3880.444780447029!2d51.40833849878181!3d35.712030047322486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e0126f6c0ef45%3A0x216fffac2aafad45!2z2LHZiNi02YY!5e0!3m2!1sen!2s!4v1642061108262!5m2!1sen!2s"}>
+                        href={"geo:35.7113,51.4105;u=35"}
+                    >
                         مسیریابی
                         &nbsp;
                         &#10095;
