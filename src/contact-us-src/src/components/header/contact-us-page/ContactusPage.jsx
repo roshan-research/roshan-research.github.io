29ae1f834044857cf082eb5d2531cecfceefd83c
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-05-08 12:08:40 +0430

    updated

--- src/contact-us-src/src/components/header/contact-us-page/ContactusPage.jsx
+++ src/contact-us-src/src/components/header/contact-us-page/ContactusPage.jsx
@@ -4,7 +4,6 @@ import {motion} from 'framer-motion';
 import emailSource from '../../../assets/images/email-icon.svg';
 import phoneSource from '../../../assets/images/phone-icon.svg';
 import locationSource from '../../../assets/images/location-icon.svg';
-import vector from '../../../assets/images/vector.svg';
 
 import {noneAnimation, startingAnimation} from "../../../animations/main-page";
 
@@ -26,12 +25,12 @@ export default function ContactusPage(props) {
         >
             <div id={'group'}>
                 <div id={'texts-links'}>
-                    <p id={'in-the-name'}>
-                        به نام خداوند جان و خرد
-                    </p>
-                    <p id={'body-text'}>
-                        <span style={{color: '#0077FF'}}>روشن </span>از سال 1390 با همت جمعی از دانشجویان دانشگاه علم و صنعت ایران شروع به کار نموده است. این گروه هدف خود را تلاش در راستای تحقق تمدن نوین اسلامی میداند و در قالب شرکت دانش‌بنیان راهکار پردازش ژرف برای پاسخ به نیازهای کشور، محصولات نرم‌افزاری ارائه می‌کند.
-                    </p>
+                    {/*<p id={'in-the-name'}>*/}
+                    {/*    به نام خداوند جان و خرد*/}
+                    {/*</p>*/}
+                    {/*<p id={'body-text'}>*/}
+                    {/*    <span style={{color: '#0077FF'}}>روشن </span>از سال 1390 با همت جمعی از دانشجویان دانشگاه علم و صنعت ایران شروع به کار نموده است. این گروه هدف خود را تلاش در راستای تحقق تمدن نوین اسلامی میداند و در قالب شرکت دانش‌بنیان راهکار پردازش ژرف برای پاسخ به نیازهای کشور، محصولات نرم‌افزاری ارائه می‌کند.*/}
+                    {/*</p>*/}
                     <div id={'email-row'}>
                         <img id={'email-icon'} src={emailSource} alt={''}/>
                         <a href={'mailto:salam@roshan-ai.ir'} className={'link-content'}>
@@ -59,7 +58,6 @@ export default function ContactusPage(props) {
                                 میدان ولیعصر، خیابان به‌آفرین، پلاک 30، طبقه دوم
                             </p>
                         </a>
-                        <img src={vector} alt={'vector'} id={'vector'}/>
                     </div>
                 </div>
                 <div id={'contact-image'}>
--- src/contact-us-src/src/components/header/header.js
+++ src/contact-us-src/src/components/header/header.js
@@ -5,6 +5,7 @@ import Navbar from "./navigation-bar/navbar";
 import {ReactComponent as HomeButton} from '../../assets/images/home-mobile.svg'
 import JoinUsButton from "./navigation-bar/links-group/join-us-button/join-us-button";
 import ContactUsButton from "./navigation-bar/links-group/contact-us-button/contact-us-button";
+import mapImage from "../../assets/images/Map.svg";
 
 const ContactUsPage = lazy(() => import("./contact-us-page/ContactusPage"));
 
@@ -47,6 +48,14 @@ export default class Header extends Component {
                         toggle={this.toggle}
                         isOpen={this.state.isOpen}
                     />
+                    <img src={mapImage} id={'map-image'} alt={''}/>
+                    <a
+                        id={'navigation-button'}
+                        href={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3880.444780447029!2d51.40833849878181!3d35.712030047322486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e0126f6c0ef45%3A0x216fffac2aafad45!2z2LHZiNi02YY!5e0!3m2!1sen!2s!4v1642061108262!5m2!1sen!2s"}>
+                        مسیریابی
+                        &nbsp;
+                        &#10095;
+                    </a>
                     {this.reactToToggles()}
                 </div>
             </div>
