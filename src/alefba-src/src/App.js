1bb883200310d801ccedc4d7fc7fbd1923ce5c9e
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-05-26 18:31:39 +0430

    adjusted the spacings in alefba

--- src/alefba-src/src/App.js
+++ src/alefba-src/src/App.js
@@ -5,25 +5,11 @@ import UsagesSection from "./components/UsagesSection";
 import TechnicalUtils from "./components/TechnicalUtils";
 import SampleSection from "./components/SampleSection";
 import Footer from "./components/Footer";
-import {WaveformVisualizer, WaveformVisualizerTheme} from 'react-audio-visualizers';
 
 function App() {
   return (
     <div className={'alefba'}>
-          <div style={{width: '100vw',height: '100vh'}}>
-                <WaveformVisualizer
-                    audio="https://www.mboxdrive.com/val1.mp3"
-                    theme={WaveformVisualizerTheme.squaredBars}
-                    colors={['#009688', '#26a69a']}
-                    iconsColor="#26a69a"
-                    backgroundColor="black"
-                    showMainActionIcon
-                    showLoaderIcon
-                    highFrequency={8000}
-                />
-          </div>
         <MainSection/>
-        <div id={'main-abilities-space'}/>
         <AbilitiesSection/>
         <div id={'abilities-usage-space'}/>
         <UsagesSection/>
