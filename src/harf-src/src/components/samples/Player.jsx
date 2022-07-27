1a49706df1ce5f18f5474f1c3da7bd5f4efacf76
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2022-07-27 11:57:32 +0430

    added button to player

--- src/harf-src/src/components/samples/Player.jsx
+++ src/harf-src/src/components/samples/Player.jsx
@@ -1,30 +1,52 @@
 import React from "react";
 import WaveSurfer from "wavesurfer.js";
-import {useEffect,useRef} from "react";
-import music from "../razor.mp3"
+import {useEffect,useRef,useState} from "react";
+import music from "./air.mp3"
+import "../../stylesheets/player.scss";
 
 const Player = () => {
-
     const waveformRef = useRef(null);
     const wavesurfer = useRef(null);
+    const [isActive,setIsActive] = useState(false);
 
     useEffect(() => {
-
         wavesurfer.current = WaveSurfer.create({
             container: waveformRef.current,
+            barGap: 1,
+            waveColor: "gray",
+            barHeight: 0.5,
+            barRadius: 1,
+            barWidth: 5,
+            cursorColor: "transparent",
+            progressColor: "#26FF7B"
         });
-
-        wavesurfer.current.setWaveColor("white");
-        wavesurfer.current.setCursorColor("white");
-        wavesurfer.current.setProgressColor("white");
         wavesurfer.current.load(music);
-        wavesurfer.current.on('ready', function () {
+        wavesurfer.current.on('ready', () => {
             wavesurfer.current.play();
         });
     },[])
 
+    const buttonAction = () => {
+        if (isActive) {
+            wavesurfer.current.pause();
+        } else {
+            wavesurfer.current.play();
+        }
+        setIsActive(!isActive);
+    };
+
     return (
-        <div ref={waveformRef} style={{width: "50vw"}}>
+        <div id={"player-container"}>
+            <div className={`botón ${isActive? "active": ""}`} onClick={buttonAction}>
+                <div className="fondo" x="0" y="0" width="200" height="200"></div>
+                <div className="icono" width="200" height="200">
+                    <div className="parte izquierda" x="0" y="0" width="200" height="200" fill="#fff"></div>
+                    <div className="parte derecha" x="0" y="0" width="200" height="200" fill="#fff"></div>
+                </div>
+                <div className="puntero"></div>
+            </div>
+            <div ref={waveformRef} style={{width: "33vw"}}>
+            </div>
         </div>
     )
 }
