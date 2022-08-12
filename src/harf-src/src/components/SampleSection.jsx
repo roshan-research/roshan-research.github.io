50cf864490c6f688be1bb3baea199a5bfc3a5afd
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2022-08-12 18:31:32 +0430

    added two samples to harf

--- src/harf-src/src/components/SampleSection.jsx
+++ src/harf-src/src/components/SampleSection.jsx
@@ -2,14 +2,14 @@ import Player from "./samples/Player";
 import {ReactComponent as Dot} from "../assets/images/samples/dot.svg";
 import {ReactComponent as Polygon} from "../assets/images/samples/polygon.svg";
 import '../stylesheets/sample-section.scss';
-import {motion} from 'framer-motion';
 import {segments,segmentFixer} from "./samples/segments.js";
 import rasmi from "../assets/voices/rasmi.mp3";
 import mohavereh from "../assets/voices/mohavereh.mp3";
 import telephone from "../assets/voices/telephone.mp3";
+import ghadimi from "../assets/voices/ghadimi.mp3";
+import bamusic from "../assets/voices/bamusic.mp3";
 
 import {Component} from "react";
-import {ImageAnimation} from "../animations/typingAnimation";
 
 
 let sampleSegments = segmentFixer(segments);
@@ -20,12 +20,12 @@ class SampleSection extends Component {
         chosenSample: 0
     }
 
-    voices = [rasmi,mohavereh,telephone];
+    voices = [rasmi,mohavereh,telephone,ghadimi,bamusic];
 
-    constructor(props: any) {
+    constructor(props) {
       super(props);
         window.setInterval(() => {
-            let randomNumber:number = Math.floor((Math.random() * 2) + 0);
+            let randomNumber = Math.floor((Math.random() * 2) + 0);
             this.setState({chosenSample:randomNumber});
 
         },60000);
@@ -36,7 +36,6 @@ class SampleSection extends Component {
     }
 
     onSampleClick = (sampleName) => {
-        console.log(this.state.chosenSample)
         this.setState({chosenSample: sampleName})
     }
 
@@ -86,26 +85,26 @@ class SampleSection extends Component {
                                 <Polygon className={this.state.chosenSample === 2 ? 'polygon' : 'no-dot'}/>
                             </div>
                         </div>
-                        {/*<div className={'button'} onClick={this.onSampleClick.bind(this,Sample.BaMusic)}>*/}
-                        {/*    <div className={'sample-row'}>*/}
-                        {/*        <p className={'white-title-sample'}*/}
-                        {/*           id={this.state.chosenSample === Sample.BaMusic ? 'active-orange-title' : ''}>*/}
-                        {/*            گفتار با موسیقی در حال پخش*/}
-                        {/*        </p>*/}
-                        {/*        <Dot className={this.state.chosenSample === Sample.BaMusic ? 'no-dot' : 'dot'}/>*/}
-                        {/*        <Polygon className={this.state.chosenSample === Sample.BaMusic ? 'polygon' : 'no-dot'}/>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
-                        {/*<div className={'button'} onClick={this.onSampleClick.bind(this,Sample.Ghadimi)}>*/}
-                        {/*    <div className={'sample-row'}>*/}
-                        {/*        <p className={'white-title-sample'}*/}
-                        {/*           id={this.state.chosenSample === Sample.Ghadimi ? 'active-orange-title' : ''}>*/}
-                        {/*            قدیمی*/}
-                        {/*        </p>*/}
-                        {/*        <Dot className={this.state.chosenSample === Sample.Ghadimi ? 'no-dot' : 'dot'}/>*/}
-                        {/*        <Polygon className={this.state.chosenSample === Sample.Ghadimi ? 'polygon' : 'no-dot'}/>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
+                        <div className={'button'} onClick={this.onSampleClick.bind(this,3)}>
+                           <div className={'sample-row'}>
+                               <p className={'white-title-sample'}
+                                  id={this.state.chosenSample === 3 ? 'active-orange-title' : ''}>
+                                   قدیمی
+                               </p>
+                                <Dot className={this.state.chosenSample === 3 ? 'no-dot' : 'dot'}/>
+                               <Polygon className={this.state.chosenSample === 3 ? 'polygon' : 'no-dot'}/>
+                            </div>
+                        </div>
+                        <div className={'button'} onClick={this.onSampleClick.bind(this,4)}>
+                           <div className={'sample-row'}>
+                               <p className={'white-title-sample'}
+                                   id={this.state.chosenSample === 4 ? 'active-orange-title' : ''}>
+                                    گفتار با موسیقی در حال پخش
+                               </p>
+                               <Dot className={this.state.chosenSample === 4 ? 'no-dot' : 'dot'}/>
+                               <Polygon className={this.state.chosenSample === 4 ? 'polygon' : 'no-dot'}/>
+                           </div>
+                        </div>
                         {/*<div className={'button'} onClick={this.onSampleClick.bind(this,Sample.Lahje)}>*/}
                         {/*    <div className={'sample-row'}>*/}
                         {/*        <p className={'white-title-sample'}*/}
