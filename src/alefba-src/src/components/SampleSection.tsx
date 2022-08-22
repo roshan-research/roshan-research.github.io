f67c8db5ae09dc8a0186efc477fc73f63d228a25
Muhammad Karbalaee <muhammad.ksht@gmail.com>
2022-08-22 03:30:40 -0700

    fixed everything in safari but samples and title and the first page title and tech utils images

--- src/alefba-src/src/components/SampleSection.tsx
+++ src/alefba-src/src/components/SampleSection.tsx
@@ -175,7 +175,7 @@ class SampleSection extends Component {
                     <div className={'sample-container'}>
                         {whichSample()}
                     </div>
-                    <div id={'buttons'} style={{direction: "rtl"}}>
+                    <div id={'buttons'}>
                         <div className={'button'}
                              onClick={this.onSampleClick.bind(this,Sample.KartMelli)}>
                             <div className={'sample-row'}>
