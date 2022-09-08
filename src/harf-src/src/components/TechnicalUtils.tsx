191d1d38680a3d8e2662dc75a2a318d519993c82
Ruhollah <ruhollahh01@gmail.com>
2022-09-08 15:09:45 +0430

    fix spacing issues on harf and alefba

--- src/harf-src/src/components/TechnicalUtils.tsx
+++ src/harf-src/src/components/TechnicalUtils.tsx
@@ -1,140 +1,179 @@
 import '../stylesheets/technical-utils.scss';
-import Left1 from "../assets/images/utils/left-1.svg";
-import Left2 from "../assets/images/utils/left-2.svg";
-import Right1 from "../assets/images/utils/right-1.svg";
-import Right2 from "../assets/images/utils/right-2.svg";
+import Left1 from '../assets/images/utils/left-1.svg';
+import Left2 from '../assets/images/utils/left-2.svg';
+import Right1 from '../assets/images/utils/right-1.svg';
+import Right2 from '../assets/images/utils/right-2.svg';
 
-import {ReactComponent as Accuracy} from "../assets/images/utils/accuracy-mobile.svg";
-import {ReactComponent as Parallel} from "../assets/images/utils/parallel-mobile.svg";
-import {ReactComponent as NLP} from "../assets/images/utils/nlp-mobile.svg";
-import {ReactComponent as Deep} from "../assets/images/utils/deep-mobile.svg";
-import {useInView} from "react-intersection-observer";
+import { ReactComponent as Accuracy } from '../assets/images/utils/accuracy-mobile.svg';
+import { ReactComponent as Parallel } from '../assets/images/utils/parallel-mobile.svg';
+import { ReactComponent as NLP } from '../assets/images/utils/nlp-mobile.svg';
+import { ReactComponent as Deep } from '../assets/images/utils/deep-mobile.svg';
+import { useInView } from 'react-intersection-observer';
 
 const TechnicalUtils = () => {
-
     const observerOptions = {
         delay: 1000,
-        triggerOnce: true
+        triggerOnce: true,
     };
 
-    const [right1Ref,right1InView] = useInView(observerOptions);
-    const [right2Ref,right2InView] = useInView(observerOptions);
-    const [left1Ref,left1InView] = useInView(observerOptions);
-    const [left2Ref,left2InView] = useInView(observerOptions);
+    const [right1Ref, right1InView] = useInView(observerOptions);
+    const [right2Ref, right2InView] = useInView(observerOptions);
+    const [left1Ref, left1InView] = useInView(observerOptions);
+    const [left2Ref, left2InView] = useInView(observerOptions);
 
-    return(
+    return (
         <div id={'tech-utils'}>
-            <p id={'utils-title'}>
-                امکانات فنی حرف
-            </p>
-            <div style={{height: '15vh'}}/>
+            <p id={'utils-title'}>امکانات فنی حرف</p>
+            <div id="technical-title-space" />
             <div id={'utils-wrapper'}>
                 <div id={'left-utils'}>
                     <div className={'util-group right-to-left'} id={'left-1'}>
-                        <Accuracy id={'accuracy'}/>
+                        <Accuracy id={'accuracy'} />
                         <div className={'util-row'}>
                             <div className={'title-stack-utils'}>
-                                <p 
-                                    className={left1InView? "black-util-text util-in-view" : "black-util-text"}
+                                <p
+                                    className={
+                                        left1InView
+                                            ? 'black-util-text util-in-view'
+                                            : 'black-util-text'
+                                    }
                                     ref={left1Ref}
-                                    data-title={"دقت و سرعت"}
+                                    data-title={'دقت و سرعت'}
                                 >
                                     دقت و سرعت
                                 </p>
-                                <p 
-                                    className={left1InView? "black-util-text-background util-in-view-bg" : "black-util-text-background"}
-                                    data-title={"دقت و سرعت"}
+                                <p
+                                    className={
+                                        left1InView
+                                            ? 'black-util-text-background util-in-view-bg'
+                                            : 'black-util-text-background'
+                                    }
+                                    data-title={'دقت و سرعت'}
                                 >
                                     دقت و سرعت
                                 </p>
                             </div>
                         </div>
-                        <img src={Left1} id={'left1-icon'} alt={''}/>
+                        <img src={Left1} id={'left1-icon'} alt={''} />
                         <p className={'white-util-text'}>
-                            حرف با دقت واژه‌ای ۹۲٪ گفتار را به متن تبدیل می‌کند و با سخت‌افزار مناسب می‌تواند یک ساعت صدا را تقریبا در زمان یک دقیقه تحلیل کند.
+                            حرف با دقت واژه‌ای ۹۲٪ گفتار را به متن تبدیل می‌کند
+                            و با سخت‌افزار مناسب می‌تواند یک ساعت صدا را تقریبا
+                            در زمان یک دقیقه تحلیل کند.
                         </p>
                     </div>
-                    <div id={'right-space'}/>
+                    <div id={'right-space'} />
                     <div className={'util-group left-to-right'} id={'right-1'}>
-                        <NLP id={'nlp'}/>
+                        <NLP id={'nlp'} />
                         <div className={'util-row left-to-right-row1'}>
                             <div className={'title-stack-utils'}>
-                                <p 
-                                    className={right1InView? "black-util-text util-in-view" : "black-util-text"}
+                                <p
+                                    className={
+                                        right1InView
+                                            ? 'black-util-text util-in-view'
+                                            : 'black-util-text'
+                                    }
                                     ref={right1Ref}
-                                    data-title={"فهم زبان فارسی"}
+                                    data-title={'فهم زبان فارسی'}
                                 >
                                     فهم زبان فارسی
                                 </p>
-                                <p 
-                                    className={right1InView? "black-util-text-background util-in-view-bg" : "black-util-text-background"}
-                                    data-title={"فهم زبان فارسی"}
+                                <p
+                                    className={
+                                        right1InView
+                                            ? 'black-util-text-background util-in-view-bg'
+                                            : 'black-util-text-background'
+                                    }
+                                    data-title={'فهم زبان فارسی'}
                                 >
                                     فهم زبان فارسی
                                 </p>
                             </div>
                         </div>
-                        <img src={Right1} id={'right1-icon'} alt={''}/>
+                        <img src={Right1} id={'right1-icon'} alt={''} />
                         <p className={'white-util-text left-to-right'}>
-                            حرف، جمله‌های فارسی را می‌شناسد. یعنی وقتی که میان چند واژه تردید دارد، واژه‌ای را انتخاب می‌کند که دیگر کلمات گفتار را بهتر تکمیل می‌کند.
+                            حرف، جمله‌های فارسی را می‌شناسد. یعنی وقتی که میان
+                            چند واژه تردید دارد، واژه‌ای را انتخاب می‌کند که
+                            دیگر کلمات گفتار را بهتر تکمیل می‌کند.
                         </p>
                     </div>
                 </div>
                 <div id={'right-utils'}>
-                    <div id={'right-space'}/>
+                    <div id={'right-space'} />
                     <div className={'util-group right-to-left'} id={'left-2'}>
-                        <Parallel id={'parallel'}/>
+                        <Parallel id={'parallel'} />
                         <div className={'util-row'}>
                             <div className={'title-stack-utils'}>
-                                <p 
-                                    className={left2InView? "black-util-text util-in-view" : "black-util-text"}
+                                <p
+                                    className={
+                                        left2InView
+                                            ? 'black-util-text util-in-view'
+                                            : 'black-util-text'
+                                    }
                                     ref={left2Ref}
-                                    data-title={"پردازش موازی"}
+                                    data-title={'پردازش موازی'}
                                 >
                                     پردازش موازی
                                 </p>
-                                <p 
-                                    className={left2InView? "black-util-text-background util-in-view-bg" : "black-util-text-background"}
-                                    data-title={"پردازش موازی"}
+                                <p
+                                    className={
+                                        left2InView
+                                            ? 'black-util-text-background util-in-view-bg'
+                                            : 'black-util-text-background'
+                                    }
+                                    data-title={'پردازش موازی'}
                                 >
                                     پردازش موازی
                                 </p>
                             </div>
                         </div>
-                        <img src={Left2} id={'left2-icon'} alt={''}/>
+                        <img src={Left2} id={'left2-icon'} alt={''} />
                         <p className={'white-util-text'}>
-                            حرف می‌تواند با استفاده از پردازنده گرافیکی (کارت گرافیک)، داده‌ها را چند برابر سریع‌تر پردازش کند. همچنین پردازش توزیع‌شده، امکان تحلیل انبوه داده‌ها را فراهم می‌کند.
+                            حرف می‌تواند با استفاده از پردازنده گرافیکی (کارت
+                            گرافیک)، داده‌ها را چند برابر سریع‌تر پردازش کند.
+                            همچنین پردازش توزیع‌شده، امکان تحلیل انبوه داده‌ها
+                            را فراهم می‌کند.
                         </p>
                     </div>
-                    <div id={'right-space'}/>
+                    <div id={'right-space'} />
                     <div className={'util-group left-to-right'} id={'right-2'}>
-                        <Deep id={'deep'}/>
+                        <Deep id={'deep'} />
                         <div className={'util-row left-to-right-row2'}>
                             <div className={'title-stack-utils'}>
-                                <p 
-                                    className={right2InView? "black-util-text util-in-view" : "black-util-text"}
+                                <p
+                                    className={
+                                        right2InView
+                                            ? 'black-util-text util-in-view'
+                                            : 'black-util-text'
+                                    }
                                     ref={right2Ref}
-                                    data-title={"یادگیری عمیق"}
+                                    data-title={'یادگیری عمیق'}
                                 >
                                     یادگیری عمیق
                                 </p>
-                                <p 
-                                    className={right2InView? "black-util-text-background util-in-view-bg" : "black-util-text-background"}
-                                    data-title={"یادگیری عمیق"}
+                                <p
+                                    className={
+                                        right2InView
+                                            ? 'black-util-text-background util-in-view-bg'
+                                            : 'black-util-text-background'
+                                    }
+                                    data-title={'یادگیری عمیق'}
                                 >
                                     یادگیری عمیق
                                 </p>
                             </div>
                         </div>
-                        <img src={Right2} id={'right2-icon'} alt={''}/>
+                        <img src={Right2} id={'right2-icon'} alt={''} />
                         <p className={'white-util-text left-to-right'}>
-                            حرف با کمک فناوری روز دنیا یعنی یادگیری عمیق، تبدیل کردن گفتار افراد مختلف به نوشته را یاد گرفته است. هوش مصنوعی با کمک این فناوری توانسته است در بعضی کارها از هوش انسانی پیشی بگیرد.
+                            حرف با کمک فناوری روز دنیا یعنی یادگیری عمیق، تبدیل
+                            کردن گفتار افراد مختلف به نوشته را یاد گرفته است.
+                            هوش مصنوعی با کمک این فناوری توانسته است در بعضی
+                            کارها از هوش انسانی پیشی بگیرد.
                         </p>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default TechnicalUtils;
--- src/harf-src/src/components/UsagesSection.tsx
+++ src/harf-src/src/components/UsagesSection.tsx
@@ -4,79 +4,101 @@ import OrganizationsImage from '../assets/images/usages/organizations.png';
 import LibrariesImage from '../assets/images/usages/libraries.png';
 import Archive from '../assets/images/usages/archive.png';
 import { useInView } from 'react-intersection-observer';
-import {isMobile} from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 
 const UsagesSection = () => {
-
     const observerOptions = {
         delay: 1000,
-        triggerOnce: true
+        triggerOnce: true,
     };
 
-    const beforeFade = isMobile? {opacity: 1}: {opacity: 0};
+    const beforeFade = isMobile ? { opacity: 1 } : { opacity: 0 };
 
     const afterFade = {
-        opacity: 1
+        opacity: 1,
     };
 
-    const [businessesRef,businessesInView] = useInView(observerOptions);
-    const [organizationsRef,organizationsInView] = useInView(observerOptions);
-    const [librariesRef,librariesInView] = useInView(observerOptions);
-    const [archiveRef,archiveInView] = useInView(observerOptions);
+    const [businessesRef, businessesInView] = useInView(observerOptions);
+    const [organizationsRef, organizationsInView] = useInView(observerOptions);
+    const [librariesRef, librariesInView] = useInView(observerOptions);
+    const [archiveRef, archiveInView] = useInView(observerOptions);
 
-    return(
+    return (
         <div>
-            <p id={'usages-title'}>
-                کاربردهای حرف
-            </p>
+            <p id={'usages-title'}>کاربردهای حرف</p>
+            <div id="usages-title-space" />
             <div id={'usages-section'}>
-                <div id={'space-1'}/>
-                <div id={'in-businesses'} ref={businessesRef} style={businessesInView? afterFade : beforeFade}>
-                    <img src={BusinessImage} id={'business-image'} alt={''}/>
-                    <p className={'orange-text-usages'}>
-                        تایپ گفتاری
-                    </p>
+                <div id={'space-1'} />
+                <div
+                    id={'in-businesses'}
+                    ref={businessesRef}
+                    style={businessesInView ? afterFade : beforeFade}
+                >
+                    <img src={BusinessImage} id={'business-image'} alt={''} />
+                    <p className={'orange-text-usages'}>تایپ گفتاری</p>
                     <p className={'white-text-usages'}>
-                        تایپ کردن متن با استفاده از دکمه‌های فراوان صفحه کلید، سخت است. در دنیای جدید با خواندن متن آن را دیجیتال می‌کنیم.
+                        تایپ کردن متن با استفاده از دکمه‌های فراوان صفحه کلید،
+                        سخت است. در دنیای جدید با خواندن متن آن را دیجیتال
+                        می‌کنیم.
                     </p>
                 </div>
                 <div>
-                    <div id={'space-2'}/>
-                    <div id={'in-organizations'} ref={organizationsRef}  style={organizationsInView? afterFade : beforeFade}>
-                        <img src={OrganizationsImage} id={'organization-image'} alt={''}/>
-                        <p className={'orange-text-usages'}>
-                            تعامل گفتاری
-                        </p>
+                    <div id={'space-2'} />
+                    <div
+                        id={'in-organizations'}
+                        ref={organizationsRef}
+                        style={organizationsInView ? afterFade : beforeFade}
+                    >
+                        <img
+                            src={OrganizationsImage}
+                            id={'organization-image'}
+                            alt={''}
+                        />
+                        <p className={'orange-text-usages'}>تعامل گفتاری</p>
                         <p className={'white-text-usages'}>
-                            ابزارهای هوشمند در شکل‌ها و اندازه‌های مختلف وارد زندگی ما می‌شوند. ارتباط کلامی با این ابزارها، راحت‌ترین راه تعامل است.
+                            ابزارهای هوشمند در شکل‌ها و اندازه‌های مختلف وارد
+                            زندگی ما می‌شوند. ارتباط کلامی با این ابزارها،
+                            راحت‌ترین راه تعامل است.
                         </p>
                     </div>
                 </div>
                 <div>
-                    <div id={'space-3'}/>
-                    <div id={'in-libraries'} ref={librariesRef}  style={librariesInView? afterFade : beforeFade}>
-                        <img src={LibrariesImage} id={'library-image'} alt={''}/>
-                        <p className={'orange-text-usages'}>
-                            پیاده‌سازی جلسات
-                        </p>
+                    <div id={'space-3'} />
+                    <div
+                        id={'in-libraries'}
+                        ref={librariesRef}
+                        style={librariesInView ? afterFade : beforeFade}
+                    >
+                        <img
+                            src={LibrariesImage}
+                            id={'library-image'}
+                            alt={''}
+                        />
+                        <p className={'orange-text-usages'}>پیاده‌سازی جلسات</p>
                         <p className={'white-text-usages'}>
-                            صحبت‌هایی که در جلسه‌های حضوری یا مجازی، رد و بدل می‌شوند، مهم هستند و برای رجوع به آنها لازم است متن هر جلسه را داشته باشیم.
+                            صحبت‌هایی که در جلسه‌های حضوری یا مجازی، رد و بدل
+                            می‌شوند، مهم هستند و برای رجوع به آنها لازم است متن
+                            هر جلسه را داشته باشیم.
                         </p>
                     </div>
                 </div>
-                <div id={'space-5'}/>
-                <div id={'in-archives'} ref={archiveRef} style={archiveInView? afterFade : beforeFade}>
-                    <img src={Archive} id={'business-image'} alt={''}/>
-                    <p className={'orange-text-usages'}>
-                        جستجو در آرشیو
-                    </p>
+                <div id={'space-5'} />
+                <div
+                    id={'in-archives'}
+                    ref={archiveRef}
+                    style={archiveInView ? afterFade : beforeFade}
+                >
+                    <img src={Archive} id={'business-image'} alt={''} />
+                    <p className={'orange-text-usages'}>جستجو در آرشیو</p>
                     <p className={'white-text-usages'}>
-                        داده‌های ویدئویی با سرعت بسیار بالا در حال زیاد شدن هستند. پیاده‌سازی متن ویدئوها می‌تواند پیدا کردن یک محتوای خاص را آسان کند.
+                        داده‌های ویدئویی با سرعت بسیار بالا در حال زیاد شدن
+                        هستند. پیاده‌سازی متن ویدئوها می‌تواند پیدا کردن یک
+                        محتوای خاص را آسان کند.
                     </p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default UsagesSection;
