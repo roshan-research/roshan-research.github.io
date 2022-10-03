66c8a36e17553d430b0ebf3835e5a5d139ec8568
Ruhollah <ruhollahh01@gmail.com>
2022-10-03 13:14:05 +0330

    fix join us page bugs

--- src/join-us-src/src/components/header/header.js
+++ src/join-us-src/src/components/header/header.js
@@ -1,86 +1,96 @@
-import React, {lazy} from 'react';
-import {Component} from 'react';
-import './header.scss'
-import Navbar from "./navigation-bar/navbar";
-import {ReactComponent as HomeButton} from '../../assets/images/home-mobile.svg'
-import JoinUsButton from "./navigation-bar/links-group/join-us-button/join-us-button";
-import ContactUsButton from "./navigation-bar/links-group/contact-us-button/contact-us-button";
+import React, { lazy } from 'react';
+import { Component } from 'react';
+import './header.scss';
+import Navbar from './navigation-bar/navbar';
+import { ReactComponent as HomeButton } from '../../assets/images/home-mobile.svg';
+import JoinUsButton from './navigation-bar/links-group/join-us-button/join-us-button';
+import ContactUsButton from './navigation-bar/links-group/contact-us-button/contact-us-button';
 import { slide as Menu } from 'react-burger-menu';
-import MenuLinks from "../MenuLinks";
+import MenuLinks from '../MenuLinks';
 import closeIcon from '../../assets/images/close-icon.svg';
 import burgerIcon from '../../assets/images/menu-icon.svg';
 
-const JoinusPage = lazy(() => import("./join-us-page/joinus-page"));
+const JoinusPage = lazy(() => import('./join-us-page/joinus-page'));
 
 class Header extends Component {
     state = {
-        isOpen:false,
+        isOpen: false,
         timesRendered: 0,
-    }
+    };
 
     toggle = () => {
         this.setState({
             isOpen: !this.state.isOpen,
-            timesRendered: this.state.timesRendered + 1
-        })
+            timesRendered: this.state.timesRendered + 1,
+        });
     };
 
     reactToToggles = () => {
         if (this.state.isOpen) {
             return (
                 <div id={'compact-menu'}>
-                    <a href={'https://www.roshan-ai.ir/'} id={'mobile-menu-link'}>
+                    <a
+                        href={'https://www.roshan-ai.ir/'}
+                        id={'mobile-menu-link'}
+                    >
                         روشن
                     </a>
-                    <a href={'https://www.roshan-ai.ir/about-us'} id={'mobile-menu-link'}>
+                    <a
+                        href={'https://www.roshan-ai.ir/about-us'}
+                        id={'mobile-menu-link'}
+                    >
                         درباره ما
                     </a>
-                    <a href={'https://www.roshan-ai.ir/about-us'} id={'mobile-menu-link'}>
+                    <a
+                        href={'https://www.roshan-ai.ir/about-us'}
+                        id={'mobile-menu-link'}
+                    >
                         تماس با ما
                     </a>
-                    <a href={'https://www.roshan-ai.ir/about-us'} id={'mobile-menu-link'}>
+                    <a
+                        href={'https://www.roshan-ai.ir/about-us'}
+                        id={'mobile-menu-link'}
+                    >
                         همکاری با ما
                     </a>
                 </div>
-            )
+            );
         } else {
-            return(
-                <JoinusPage shouldRerender={this.state.timesRendered === 0}/>
-            )
+            return (
+                <JoinusPage shouldRerender={this.state.timesRendered === 0} />
+            );
         }
-    }
+    };
 
     render() {
-        return(
-            <div id={'all'}>
+        return (
+            <div
+                id={'all'}
+                style={{ background: this.state.isOpen && 'black' }}
+            >
                 <Menu
-                    menuClassName={ "menu" }
+                    menuClassName={'menu'}
                     itemListElement="div"
-                    burgerButtonClassName={ "my-button" }
-                    pageWrapId={ "header-wrapper" }
-                    itemListClassName={ "menu-items" }
-                    crossButtonClassName={ "my-cross" }
-                    outerContainerId={ "all" }
-                    customBurgerIcon={
-                        <img src={burgerIcon} alt={''}/>
-                    }
+                    burgerButtonClassName={'my-button'}
+                    pageWrapId={'header-wrapper'}
+                    itemListClassName={'menu-items'}
+                    crossButtonClassName={'my-cross'}
+                    outerContainerId={'all'}
+                    customBurgerIcon={<img src={burgerIcon} alt={''} />}
                     customCrossIcon={
                         <div>
-                            <img src={closeIcon} alt={''}/>
+                            <img src={closeIcon} alt={''} />
                         </div>
                     }
                 >
-                    <MenuLinks/>
+                    <MenuLinks />
                 </Menu>
                 <div className="header-wrapper" id={'header-wrapper'}>
-                    <Navbar
-                        toggle={this.toggle}
-                        isOpen={this.state.isOpen}
-                    />
+                    <Navbar toggle={this.toggle} isOpen={this.state.isOpen} />
                     {this.reactToToggles()}
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- src/join-us-src/src/components/header/join-us-page/joinus-page.js
+++ src/join-us-src/src/components/header/join-us-page/joinus-page.js
@@ -1,56 +1,60 @@
 import React from 'react';
-import {ReactComponent as JoinUsMobile} from "../../../assets/images/mobile-join-us.svg";
-import {ReactComponent as JoinUsText} from "../../../assets/images/join-us-text-group.svg";
+import { ReactComponent as JoinUsMobile } from '../../../assets/images/mobile-join-us.svg';
+import { ReactComponent as JoinUsText } from '../../../assets/images/join-us-text-group.svg';
 
-import  Jobinja from "../../../assets/images/jobinja.svg";
-import Linkedin from "../../../assets/images/linkedin.svg";
-import Mail from "../../../assets/images/mail.svg";
+import Jobinja from '../../../assets/images/jobinja.svg';
+import Linkedin from '../../../assets/images/linkedin.svg';
+import Mail from '../../../assets/images/mail.svg';
 
 import './joinus-page.scss';
-import {isMobile,isTablet} from "react-device-detect";
+import { isMobile, isTablet } from 'react-device-detect';
 
 const returnBasedOneDevice = () => {
-    if(isMobile || isTablet){
-        return(
-            <div id={ 'mobile-j'}>
-                <JoinUsMobile id={'pic'}/>
+    if (isMobile || isTablet) {
+        return (
+            <div id={'mobile-j'}>
+                <JoinUsMobile id={'pic'} />
+                <div className="mobile-social">
+                    <a href="mailto:salam@roshan-ai.ir">
+                        <img src={Mail} alt="" width="76" height="34" />
+                    </a>
+                    <a href="https://jobinja.ir/companies/roshan-ai/jobs">
+                        <img src={Linkedin} alt="" width="76" height="34" />
+                    </a>
+                    <a href="https://www.linkedin.com/company/roshan-ai/">
+                        <img src={Jobinja} alt="" width="76" height="34" />
+                    </a>
+                </div>
             </div>
-        )
+        );
     } else {
-        return(
+        return (
             <div id={'web'}>
                 <div id={'text-group'}>
-                    <JoinUsText id={'inner'}/>
+                    <JoinUsText id={'inner'} />
                     <div id={'button-row'}>
                         <a
                             className={'join-us-link-button'}
                             href={'https://jobinja.ir/companies/roshan-ai/jobs'}
                             id={'jobinja-button'}
-                        >
-                        </a>
+                        ></a>
                         <a
                             className={'join-us-link-button'}
-                           href={'https://www.linkedin.com/company/roshan-ai/'}
+                            href={'https://www.linkedin.com/company/roshan-ai/'}
                             id={'linkedin-button'}
-                        >
-                        </a>
+                        ></a>
                         <a
                             className={'join-us-link-button'}
                             href={'mailto:salam@roshan-ai.ir'}
                             id={'mail-button'}
-                        >
-                        </a>
+                        ></a>
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 };
 
 export default function JoinusPage() {
-    return (
-        <div className={'join-us-page'}>
-            {returnBasedOneDevice()}
-        </div>
-    )
+    return <div className={'join-us-page'}>{returnBasedOneDevice()}</div>;
 }
