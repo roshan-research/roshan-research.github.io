3a2b57e51ec1001a7ba8128f409874094d8e6c9d
Ruhollah <ruhollahh01@gmail.com>
2022-10-11 16:36:28 +0330

    deploy

--- src/contact-us-src/src/components/header/header.js
+++ src/contact-us-src/src/components/header/header.js
@@ -1,96 +1,112 @@
-import React, {lazy} from 'react';
-import {Component} from 'react';
+import React, { lazy } from 'react';
+import { Component } from 'react';
 import './header.scss';
-import Navbar from "./navigation-bar/navbar";
-import {ReactComponent as HomeButton} from '../../assets/images/home-mobile.svg'
-import JoinUsButton from "./navigation-bar/links-group/join-us-button/join-us-button";
-import ContactUsButton from "./navigation-bar/links-group/contact-us-button/contact-us-button";
-import mapImage from "../../assets/images/Map.svg";
+import Navbar from './navigation-bar/navbar';
+import { ReactComponent as HomeButton } from '../../assets/images/home-mobile.svg';
+import JoinUsButton from './navigation-bar/links-group/join-us-button/join-us-button';
+import ContactUsButton from './navigation-bar/links-group/contact-us-button/contact-us-button';
+import mapImage from '../../assets/images/map.svg';
 import { slide as Menu } from 'react-burger-menu';
-import isMobile from 'react-device-detect'
+import isMobile from 'react-device-detect';
 import closeIcon from '../../assets/images/close-icon.svg';
 import burgerIcon from '../../assets/images/menu-icon.svg';
-import MenuLinks from "../MenuLinks";
+import MenuLinks from '../MenuLinks';
 
-const ContactUsPage = lazy(() => import("./contact-us-page/ContactusPage"));
+const ContactUsPage = lazy(() => import('./contact-us-page/ContactusPage'));
 
 export default class Header extends Component {
     state = {
-        isOpen:false,
+        isOpen: false,
         timesRendered: 0,
-    }
+    };
 
     toggle = () => {
         this.setState({
             isOpen: !this.state.isOpen,
-            timesRendered: this.state.timesRendered + 1
-        })
+            timesRendered: this.state.timesRendered + 1,
+        });
     };
 
     reactToToggles = () => {
         if (this.state.isOpen) {
             return (
                 <div id={'compact-menu'}>
-                    <a href={'https://www.roshan-ai.ir/'} id={'mobile-menu-link'}>
+                    <a
+                        href={'https://www.roshan-ai.ir/'}
+                        id={'mobile-menu-link'}
+                    >
                         صفحهٔ اصلی
                     </a>
-                    <a href={'https://www.roshan-ai.ir/about-us'} id={'mobile-menu-link'}>
+                    <a
+                        href={'https://www.roshan-ai.ir/about-us'}
+                        id={'mobile-menu-link'}
+                    >
                         دربارهٔ روشن
                     </a>
-                    <a href={'https://www.roshan-ai.ir/about-us'} id={'mobile-menu-link'}>
+                    <a
+                        href={'https://www.roshan-ai.ir/about-us'}
+                        id={'mobile-menu-link'}
+                    >
                         تماس با ما
                     </a>
-                    <a href={'https://www.roshan-ai.ir/about-us'} id={'mobile-menu-link'}>
+                    <a
+                        href={'https://www.roshan-ai.ir/about-us'}
+                        id={'mobile-menu-link'}
+                    >
                         همکاری با ما
                     </a>
                 </div>
-            )
+            );
         } else {
-            return(
-                <ContactUsPage shouldRerender={this.state.timesRendered === 0}/>
-            )
+            return (
+                <ContactUsPage
+                    shouldRerender={this.state.timesRendered === 0}
+                />
+            );
         }
-    }
+    };
 
     render() {
-        return(
+        return (
             <div id={'all'}>
                 <Menu
-                    menuClassName={ "menu" }
+                    menuClassName={'menu'}
                     itemListElement="div"
-                    burgerButtonClassName={ "my-button" }
-                    pageWrapId={ "header-wrapper" }
-                    itemListClassName={ "menu-items" }
-                    crossButtonClassName={ "my-cross" }
-                    outerContainerId={ "all" }
-                    customBurgerIcon={
-                        <img src={burgerIcon} alt={''}/>
-                    }
+                    burgerButtonClassName={'my-button'}
+                    pageWrapId={'header-wrapper'}
+                    itemListClassName={'menu-items'}
+                    crossButtonClassName={'my-cross'}
+                    outerContainerId={'all'}
+                    customBurgerIcon={<img src={burgerIcon} alt={''} />}
                     customCrossIcon={
                         <div>
-                            <img src={closeIcon} alt={''}/>
+                            <img src={closeIcon} alt={''} />
                         </div>
                     }
                 >
-                    <MenuLinks/>
+                    <MenuLinks />
                 </Menu>
                 <div className="header-wrapper" id={'header-wrapper'}>
-                    <Navbar
-                        toggle={this.toggle}
-                        isOpen={this.state.isOpen}
+                    <Navbar toggle={this.toggle} isOpen={this.state.isOpen} />
+                    <img
+                        src={mapImage}
+                        className={this.state.isOpen ? 'hidden-image' : ''}
+                        id={'map-image'}
+                        alt={''}
                     />
-                    <img src={mapImage} className={this.state.isOpen ? 'hidden-image': ''} id={'map-image'} alt={''}/>
                     <a
                         id={'navigation-button'}
-                        href={isMobile? "geo:35.7113,51.4105;u=35" : "https://goo.gl/maps/ms3rUPvSMtsscEXr9"}
+                        href={
+                            isMobile
+                                ? 'geo:35.7113,51.4105;u=35'
+                                : 'https://goo.gl/maps/ms3rUPvSMtsscEXr9'
+                        }
                     >
-                        مسیریابی
-                        &nbsp;
-                        &#10095;
+                        مسیریابی &nbsp; &#10095;
                     </a>
                     {this.reactToToggles()}
                 </div>
             </div>
-        )
+        );
     }
 }
