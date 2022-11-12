d73813515646efe8a6504e19e6820f26b5206723
Ruhollah <ruhollahh01@gmail.com>
2022-11-13 00:42:50 +0330

    clean up old code

--- src/alefba-src/src/components/SampleSection.tsx
+++ src/alefba-src/src/components/SampleSection.tsx
@@ -24,7 +24,7 @@ enum Sample {
 
 class SampleSection extends Component {
     state = {
-        chosenSample: Sample.KartMelli,
+        chosenSample: Sample.BiKeifyat,
         kartMelliKey: 'kartMelli',
         fishKey: 'fish',
         charkheshKey: 'charkhesh',
