3ec28a9238e111ac466cc0d3f42f9d9452c31168
Ruhollah <ruhollahh01@gmail.com>
2022-11-22 20:38:01 +0330

    deploy join us

--- src/join-us-src/src/components/header/join-us-page/joinus-page.js
+++ src/join-us-src/src/components/header/join-us-page/joinus-page.js
@@ -12,7 +12,7 @@ export default function JoinusPage() {
         <div className={'join-us-page'}>
             <div id={'wrapper'}>
                 <div className='text-wrapper'>
-                    <p>ss</p>                    
+                    <p>اگر به هوش مصنوعی و چالش‌های جذابِ آن</p>                    
                     <p>و کار در یک محیط جوان، صمیمی و پویا علاقه دارید،</p>
                     <p>جای درستی آمده‌اید.</p>
                 </div>
