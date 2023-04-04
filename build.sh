4d262acee78573a22c73cd90ad81017ea0290e74
Ayub Kokabi <sir.kokabi@gmail.com>
2023-04-04 15:23:30 +0330

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
-root=`git rev-parse --show-toplevel`
+root=$(git rev-parse --show-toplevel)
 
-build(){                   
-    src=$root/src/$1-src/      
-    cd $src
+build() {
+    src="$root/src/$1-src/"
+    cd "$src"
     rm -rf node_modules package-lock.json
     npm install --legacy-peer-deps && npm run build
 
-    if [ $1 = "main" ]; then              
-        cp -r $src/build/* $root/
-    else    
-        rm -rf $root/$1/
-        mkdir $root/$1/  
-        cp -r $src/build/* $root/$1/    
+    if [ "$1" = "main" ]; then
+        cp -r "$src/build/"* "$root/"
+    else
+        rm -rf "$root/$1/"
+        mkdir "$root/$1/"
+        cp -r "$src/build/"* "$root/$1/"
     fi
 }
 
-products=("main" "about-us" "join-us" "contact-us" "alefba" "harf")            
+products=("main" "about-us" "join-us" "contact-us" "alefba" "harf")
 
-for product in ${products[@]}; do
-    build $product
+for product in "${products[@]}"; do
+    build "$product"
 done
