02c0cce50a32e7a54c87af7a14840bf940502e3e
mohammad khosrotabar <mh.khosrotabar75@gmail.com>
2023-12-13 12:24:46 +0330

    change conclusion face recognize image

--- src/components/conclusion/face-recognize/FaceRecognize.tsx
+++ src/components/conclusion/face-recognize/FaceRecognize.tsx
@@ -20,10 +20,11 @@ const FaceRecognize: FC<FaceRecognizeProps> = ({ activeTab }) => {
         viewport={{ once: true, amount: 0.5 }}
         className={clsx(
           "relative h-[470px] w-[770px] custombp5:hidden custombp5:h-auto custombp5:w-[95%]",
-          activeTab === 2 ? "block" : "hidden",
+          activeTab === 2 ? "block" : "hidden"
         )}
-        onViewportEnter={() => setInview(true)}>
-        <div className='absolute right-[295px] top-[95px] h-fit w-fit'>
+        onViewportEnter={() => setInview(true)}
+      >
+        <div className='absolute right-[308px] top-[85px] h-fit w-fit'>
           <Animation
             className={[
               "absolute -top-[175px] left-[98px] overflow-visible custombp5:-right-[40px] custombp5:-top-[30px]",
@@ -32,7 +33,7 @@ const FaceRecognize: FC<FaceRecognizeProps> = ({ activeTab }) => {
               "absolute -top-[174px] left-[215px] h-[36px] w-[136px] bg-[#a86cf333]",
               "absolute top-[40px] whitespace-nowrap text-sm text-white",
             ]}
-            width={[120, 270]}
+            width={[130, 270]}
             height={[150, 190]}
             duration={[
               {
@@ -82,7 +83,8 @@ const FaceRecognize: FC<FaceRecognizeProps> = ({ activeTab }) => {
       {/* responsive */}
       <motion.div
         className='hidden h-[470px] w-[770px] sm:mt-[153px] custombp5:flex custombp5:h-auto custombp5:w-full custombp5:items-center custombp5:justify-center'
-        onViewportEnter={() => setInview(true)}>
+        onViewportEnter={() => setInview(true)}
+      >
         <div className='relative h-fit w-fit'>
           <div className='absolute left-[45.5%] top-[16%] h-fit w-fit'>
             <Animation
