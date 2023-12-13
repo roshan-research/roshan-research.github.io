92ade19bf65464f9417adb4e91f65e1f123438d0
mohammad khosrotabar <mh.khosrotabar75@gmail.com>
2023-12-13 11:54:15 +0330

    change main article image

--- src/components/article-main/ArticleMain.tsx
+++ src/components/article-main/ArticleMain.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
-import Butterfly from "../icons/butterfly";
+// import Butterfly from "../icons/butterfly";
 import { main_animation } from "@/components/animations/main";
+import { MainPageImage } from "../svg";
 
 const ArticleMain = () => {
   return (
@@ -8,7 +9,7 @@ const ArticleMain = () => {
       variants={main_animation}
       initial='hidden'
       animate='visible'
-      className='mx-auto mt-[25vh] flex max-w-[1200px] items-center justify-between sm:gap-[100px] custombp2:mt-[15vh] custombp2:flex-col'
+      className='mx-auto mt-[25vh] flex max-w-[1200px] items-center justify-center gap-[292px] sm:gap-[100px] custombp2:mt-[15vh] custombp2:flex-col'
     >
       <div className='flex flex-col items-start whitespace-nowrap sm:items-center'>
         <span className='text-[2vw] font-normal text-[#A76CF3] sm:text-[32px] custombp2:text-[30px]'>
@@ -18,7 +19,8 @@ const ArticleMain = () => {
           آموزش ماشین؛ به همین راحتی!
         </span>
       </div>
-      <Butterfly width={551} height={551} />
+      {/* <Butterfly width={551} height={551} /> */}
+      <MainPageImage width={317} height={239} />
     </motion.div>
   );
 };
