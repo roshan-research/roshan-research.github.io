b8fc9d76b103f4bd2002e8e0c2f6ecf52e3b13a4
khosro35 <mh.khosrotabar75@gmail.com>
2024-07-28 09:56:36 +0400

    fix animationa and inage load in first page

--- src/main-src/src/components/home-parts/HomePart1.tsx
+++ src/main-src/src/components/home-parts/HomePart1.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 
 import { variants } from "@/animations/variant";
 import earhWallpaper from "../../assets/images/wallpaper.e4312dfe1d9f19418f65.png";
+import clsx from "clsx";
 
 type HomePart1Props = {
   setActiveIndex: Dispatch<number>;
@@ -33,35 +34,36 @@ const HomePart1: React.FC<HomePart1Props> = ({ setActiveIndex }) => {
 
   return (
     <div className='w-full h-[100vh] flex items-center justify-center bg-gradient-to-t from-[#000000] to-[#202020]'>
-      {showAnimation ? (
-        <lottie-player
-          id='firstLottie'
-          ref={lottieRef}
-          autoPlay
-          src='https://lottie.host/20c9fc73-c936-4bf5-85ee-7e02118911d0/Zpg2yYw1cD.json'
-        ></lottie-player>
-      ) : (
-        <motion.div
-          variants={variants}
-          initial='hidden'
-          animate='visible'
-          className='w-full h-full relative flex items-center justify-center'
-        >
-          <Image
-            // src='static/media/wallpaper.e4312dfe1d9f19418f65.png'
-            src={earhWallpaper}
-            alt='سایت روشن - هوش مصنوعی'
-            className='w-full h-full'
-            priority={true}
-          />
-          <span
-            className='absolute text-[1.9vw] -translate-y-[80px] text-[#E5E5E5] whitespace-nowrap font-normal bp960:text-[80px] bp800:text-[60px] bp480:text-[10vw]'
-            ref={ref}
-          >
-            انسان به توان ماشیـن
-          </span>
-        </motion.div>
+      {showAnimation && (
+        <div className='absolute'>
+          <lottie-player
+            id='firstLottie'
+            ref={lottieRef}
+            autoPlay
+            src='https://lottie.host/20c9fc73-c936-4bf5-85ee-7e02118911d0/Zpg2yYw1cD.json'
+          ></lottie-player>
+        </div>
       )}
+      <div
+        className={clsx(
+          "w-full h-full relative flex transition-all duration-500 items-center justify-center opacity-0",
+          showAnimation ? "opacity-0 pointer-events-none" : "opacity-100"
+        )}
+      >
+        <Image
+          // src='static/media/wallpaper.e4312dfe1d9f19418f65.png'
+          src={earhWallpaper}
+          alt='سایت روشن - هوش مصنوعی'
+          className='w-full h-full'
+          priority={true}
+        />
+        <span
+          className='absolute text-[1.9vw] -translate-y-[80px] text-[#E5E5E5] whitespace-nowrap font-normal bp960:text-[80px] bp800:text-[60px] bp480:text-[10vw]'
+          ref={ref}
+        >
+          انسان به توان ماشیـن
+        </span>
+      </div>
     </div>
   );
 };
